2982f5b5e9a2924c21f999e463280e23389f11d1
snada <stefano.nada@gmail.com>
2016-04-18 20:02:46 +0200

    [16] App Capified fix #16

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require 'dotenv'
+Dotenv.load
+
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'shortify'
+set :repo_url, 'git@github.com:snada/shortify.git'
+
+set :deploy_to, ENV["SERVER_DEPLOY_FOLDER"]
+
+# Select ruby version
+set :rvm_ruby_version, proc { `cat .ruby-version`.chomp }
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.env')
+
+set :keep_releases, 3
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
